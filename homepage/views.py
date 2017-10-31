77b6cd1ece2120f9dbbf8fda32e651fd521fdd0d
akshay <you@example.com>
2017-10-31 15:14:43 +0530

    4

--- homepage/views.py
+++ homepage/views.py
@@ -27,6 +27,8 @@ class CandidateFormView(View):
         else:
             pass
         if(form.is_valid()):
+            import pdb
+            pdb.set_trace()
             form.save()
             return render(request,'homepage/response.html')
         else:
